142ff69904b053ad7c0b62a5f2e6dd7c0d872ae8
Gayan Justo de Moraes <gayan.moraes@compasso.com.br>
2021-09-09 20:08:20 -0300

    initial commit

---
+++ atividade_multithreading/atividade_multithreading/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace atividade_multithreading
+{
+    interface IDataService
+    {
+        Task<IEnumerable<Classe>> ObterClassesAsync();
+        Task<IEnumerable<int>> ObterIdsFiltradosAsync();
+        Task<IEnumerable<Atributos>> ObterAtributosDeClasseAsync();
+    }
+
+    class ClassesRoot
+    {
+        public IEnumerable<Classe> Classes { get; set; }
+    }
+    class IdsRoot
+    {
+        public IEnumerable<int> Ids { get; set; }
+    }
+    class AtributosRoot
+    {
+        public IEnumerable<Atributos> Atributos { get; set; }
+    }
+    class Classe
+    {
+        public int Id { get; set; }
+        public string NomeClasse { get; set; }
+        public Atributos Atributos { get; set; }
+    }
+
+    public class Atributos
+    {
+        public int ClasseId{ get; set; }
+        public int Forca { get; set; }
+        public int Destreza { get; set; }
+        public int Inteligencia { get; set; }
+    }
+
+    class Program
+    {
+       
+        static async Task Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
